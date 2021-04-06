c8a76a71cdf3924cc640a4320ec9ac6c4c3b32e1
tijmentij <36512390+tijmentij@users.noreply.github.com>
2021-04-06 18:34:14 +0200

    added Modlist

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 import imageLoader from "./imageData/imageLoaderClass.js"
 
-const images = new imageLoader()
+const images = new imageLoader();
 
-images.loadJsonGroup([]).then(() => {
-  const script = document.createElement('script')
-  script.src = "everybodyedits.js"
-  script.type = "module"
-  script.nodeValue
-  document.head.append(script)
-})
+function loadMain() {
+  const script = document.createElement('script');
+  script.src = "everybodyedits.js";
+  script.type = "module";
+  script.nodeValue;
+  document.head.append(script);
+}
 
-const randomString = "EveryBody" + Array(20).fill().reduce(string => string + "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random())], "")
-window.randomString = randomString
-window[randomString] = { images }
+fetch("./mods/modList.json").then(value => value.json()).then(json =>
+  images.loadJsonGroup(
+    Object.keys(json).map(key => json[key])
+  ).then(loadMain)
+).catch(loadMain)
+
+const randomString = "EveryBody" + String.fromCodePoint([...Array(20)].map(() => Math.floor(48 + 74 * Math.random())));
+window.randomString = randomString;
+window[randomString] = { images };
